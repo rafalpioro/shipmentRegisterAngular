55f68503303c9676eee163fbcd5c7a4a9874816e
Rafał Pióro <rafal.pioro@o2.pl>
2019-07-06 21:07:48 +0200

    added all recipients view + edit, delete with role authorization

---
+++ src/app/recipient/add-recipient/add-recipient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-recipient',
+  templateUrl: './add-recipient.component.html',
+  styleUrls: ['./add-recipient.component.css']
+})
+export class AddRecipientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
