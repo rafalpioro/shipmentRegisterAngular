1897b64d46b375360ce1488df48dd87b0597fcac
Rafał Pióro <rafal.pioro@o2.pl>
2019-07-30 18:36:37 +0200

    edit user component Added

--- src/app/admin/user/edit-user/edit-user.component.ts
+++ src/app/admin/user/edit-user/edit-user.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {CarrierType} from "../../../model/carrier-type";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {CarrierTypeApiService} from "../../carrier-type/carrier-type-api.service";
+import {Carrier} from "../../../model/carrier";
+import {Role} from "../../../model/role";
+import {RoleApiService} from "../../role/role-api.service";
+import {User} from "../../../model/user";
+import {UniqueUserValidator} from "../unique-user-validator.directive";
 
 @Component({
   selector: 'app-edit-user',
@@ -7,9 +16,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  roles: Role[];
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private dialogRef: MatDialogRef<EditUserComponent>,
+              private roleService: RoleApiService,
+              @Inject(MAT_DIALOG_DATA) public user: User) {
+
+    roleService.allRoles().subscribe(value => this.roles = value);
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm(){
+    this.form = this.fb.group({
+      name: [this.user.name, Validators.required ],
+      email: [this.user.email, Validators.required  ],
+      active: [this.user.active,Validators.required],
+      role: [this.user.role, Validators.required],
+    });
+
+  }
+
+  save(){
+    this.dialogRef.close(this.form.value);
+  }
+
+  close(){
+    this.dialogRef.close();
   }
 
 }
